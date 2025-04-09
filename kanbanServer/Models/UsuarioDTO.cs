aa9b524136dff9acf4d1dc029e631a0ca402da5b
Macarena Parra <mdp_parra@hotmail.com>
2025-04-08 21:32:25 -0600

    Solo agrege dos clases en Models

---
+++ kanbanServer/Models/UsuarioDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kanbanServer.Models
+{
+     public  class UsuarioDTO
+    {
+        public string Nombre { get; set; }
+    }
+}
