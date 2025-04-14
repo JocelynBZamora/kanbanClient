d20d19ac2168cfefe143ca087e4020d9694ff012
Macarena Parra <mdp_parra@hotmail.com>
2025-04-14 11:49:20 -0600

    Models y services

---
+++ kanbanServer/Services/KanbanServer.cs
@@ -0,0 +1,81 @@
+﻿using kanbanServer.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kanbanServer.Services
+{
+    public class KanbanServer
+    {
+        HttpListener servidorbb = new();
+        byte[] TareaIndex;
+        public event Action<ListaTareasDTO>? Tarearesibida, TareaTerminada, TareaProseso, TareaEn;
+        public KanbanServer() { TareaIndex = File.ReadAllBytes("assest/index.html"); }
+        public void Inicar()
+        {
+            if (!servidorbb.IsListening)
+            {
+                servidorbb = new();
+                servidorbb.Prefixes.Add("http://*:19800/kanban/");
+                servidorbb.Start();
+                new Thread(Escuchar) { IsBackground = true }.Start();
+            }
+        }
+        public void Escuchar()
+        {
+            try
+            {
+                var context = servidorbb.GetContext();
+                new Thread(Escuchar) { IsBackground = true }.Start();
+                if (context.Request.HttpMethod == "GET")
+                {
+                    switch (context.Request.RawUrl)
+                    {
+                        case "/kanban/":
+                            context.Response.ContentLength64 = TareaIndex.Length;
+                            context.Response.ContentType = "text/html";
+                            context.Response.StatusCode = 200;
+                            context.Response.OutputStream.Write(TareaIndex, 0, TareaIndex.Length);
+                            break;
+                        default:
+                            context.Response.StatusCode = 404;
+                            break;
+                    }
+                }
+                else if (context.Request.HttpMethod == "POST")
+                {
+                    switch (context.Request.RawUrl)
+                    {
+                        case "/kanban/nuevo":
+
+                            break;
+                        case "/kanban/pendiente":
+                            break;
+                        case "/kanban/en":
+                            break;
+                        case "/kanban/terminada":
+                            break;
+                        default:
+                            context.Response.StatusCode = (int)HttpStatusCode.NotFound; break;
+                    }
+                }
+                context.Response.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void Detener()
+        {
+            if (servidorbb.IsListening)
+            {
+                servidorbb.Stop();
+            }
+        }
+    }
+}
---
+++ kanbanServer/ViewModels/KanbanViewModels.cs
@@ -0,0 +1,61 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using kanbanServer.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace kanbanServer.ViewModels
+{
+    public class KanbanViewModels : ObservableObject
+    {
+        KanbanServer serverbb = new();
+        public ICommand IniciarCommand { get; }
+        public ICommand DetenerCommand { get; }
+        public KanbanViewModels()
+        {
+            IniciarCommand = new RelayCommand(Iniciar);
+            DetenerCommand = new RelayCommand(Detener);
+        }
+        void Iniciar()
+        {
+            try
+            {
+                serverbb.Inicar();
+            }
+            catch (HttpListenerException ex)
+            {
+                if (ex.Message.StartsWith("Acceso no autorizado"))
+                {
+                    ProcessStartInfo p = new ProcessStartInfo
+                    {
+                        FileName = "netsh.exe",
+                        Arguments = "http add urlacl url=http://*:19800/kanban/ user=Everyone",
+                        UseShellExecute = true,
+                        CreateNoWindow = false,
+                        Verb = "runas"
+                    };
+                    var res = Process.Start(p);
+                    if (res != null)
+                    {
+                        res.WaitForExit();
+                        if (res.ExitCode == 0)
+                        {
+                            serverbb.Inicar();
+                        }
+                    }
+                }
+            }
+        }
+        void Detener()
+        {
+
+            serverbb.Detener();
+        }
+    }
+}
