c5c95dfaf31cbafbc05733bbccd6ec625ad9d4dc
Jocelyn Berenice Zamora De La Cerda <201G0293@rcarbonifera.tecnm.mx>
2025-04-23 18:55:31 -0600

    helpers y enlaces

---
+++ kanbanServer/Helper/ComunicacionHelper.cs
@@ -0,0 +1,38 @@
+﻿using kanbanServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kanbanServer.Helper
+{
+    public static class ComunicacionHelper
+    {
+        public static event Action<ListaTareasDTO>? Tarearesibida,
+            TareaTerminada,
+            TareaProseso,
+            actualizar,
+            eliminar;
+        public static void NotificarTareaRecibida(ListaTareasDTO tarea)
+        {
+            Tarearesibida?.Invoke(tarea);
+        }
+        public static void NotificarTareaTerminada(ListaTareasDTO tarea)
+        {
+            TareaTerminada?.Invoke(tarea);
+        }
+        public static void NotificarTareaEnProceso(ListaTareasDTO tarea)
+        {
+            TareaProseso?.Invoke(tarea);
+        }
+        public static void NotificarActualizar(ListaTareasDTO tarea)
+        {
+            actualizar?.Invoke(tarea);
+        }
+        public static void NotificarEliminar(ListaTareasDTO tarea)
+        {
+            eliminar?.Invoke(tarea);
+        }
+    }
+}
---
+++ kanbanServer/ViewModels/ListaTareasViewModel.cs
@@ -0,0 +1,49 @@
+﻿using kanbanServer.Helper;
+using kanbanServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kanbanServer.ViewModels
+{
+    public class ListaTareasViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<ListaTareasDTO> Tareas { get; set; }
+        public ListaTareasViewModel()
+        {
+
+            Tareas = new();
+            ComunicacionHelper.Tarearesibida += (tarea) =>
+            {
+                Tareas.Add(tarea);
+            };
+
+            ComunicacionHelper.TareaProseso += (tarea) =>
+            {
+
+                Tareas.Where(x => x.Titulo == tarea.Titulo).First().Estado = (EstadoTarea)1;
+
+            };
+            ComunicacionHelper.eliminar += (tarea) =>
+            {
+                try
+                {
+                    Tareas.Remove(Tareas.Where(x => x.Titulo == tarea.Titulo).First());
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            };
+
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
+
